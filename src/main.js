8304df4e83060040225de057a73d80b2ba8db461
Paulo Silva <paulofernando78@gmail.com>
2025-08-11 02:05:31 -0300

    wip

---
+++ src/main.js
@@ -0,0 +1,429 @@
+const multiplication = [
+  {
+    container: [
+      {
+        multiply: "2 x 1",
+        answer: "",
+      },
+      {
+        multiply: "2 x 2",
+        answer: "",
+      },
+      {
+        multiply: "2 x 3",
+        answer: "",
+      },
+      {
+        multiply: "2 x 4",
+        answer: "",
+      },
+      {
+        multiply: "2 x 5",
+        answer: "",
+      },
+      {
+        multiply: "2 x 6",
+        answer: "",
+      },
+      {
+        multiply: "2 x 7",
+        answer: "",
+      },
+      {
+        multiply: "2 x 8",
+        answer: "",
+      },
+      {
+        multiply: "2 x 9",
+        answer: "",
+      },
+      {
+        multiply: "2 x 10",
+        answer: "",
+      },
+    ],
+  },
+  {
+    container: [
+      {
+        multiply: "3 x 1",
+        answer: "",
+      },
+      {
+        multiply: "3 x 2",
+        answer: "",
+      },
+      {
+        multiply: "3 x 3",
+        answer: "",
+      },
+      {
+        multiply: "3 x 4",
+        answer: "",
+      },
+      {
+        multiply: "3 x 5",
+        answer: "",
+      },
+      {
+        multiply: "3 x 6",
+        answer: "",
+      },
+      {
+        multiply: "3 x 7",
+        answer: "",
+      },
+      {
+        multiply: "3 x 8",
+        answer: "",
+      },
+      {
+        multiply: "3 x 9",
+        answer: "",
+      },
+      {
+        multiply: "3 x 10",
+        answer: "",
+      },
+    ],
+  },
+  {
+    container: [
+      {
+        multiply: "4 x 1",
+        answer: "",
+      },
+      {
+        multiply: "4 x 2",
+        answer: "",
+      },
+      {
+        multiply: "4 x 3",
+        answer: "",
+      },
+      {
+        multiply: "4 x 4",
+        answer: "",
+      },
+      {
+        multiply: "4 x 5",
+        answer: "",
+      },
+      {
+        multiply: "4 x 6",
+        answer: "",
+      },
+      {
+        multiply: "4 x 7",
+        answer: "",
+      },
+      {
+        multiply: "4 x 8",
+        answer: "",
+      },
+      {
+        multiply: "4 x 9",
+        answer: "",
+      },
+      {
+        multiply: "4 x 10",
+        answer: "",
+      },
+    ],
+  },
+  {
+    container: [
+      {
+        multiply: "5 x 1",
+        answer: "",
+      },
+      {
+        multiply: "5 x 2",
+        answer: "",
+      },
+      {
+        multiply: "5 x 3",
+        answer: "",
+      },
+      {
+        multiply: "5 x 4",
+        answer: "",
+      },
+      {
+        multiply: "5 x 5",
+        answer: "",
+      },
+      {
+        multiply: "5 x 6",
+        answer: "",
+      },
+      {
+        multiply: "5 x 7",
+        answer: "",
+      },
+      {
+        multiply: "5 x 8",
+        answer: "",
+      },
+      {
+        multiply: "5 x 9",
+        answer: "",
+      },
+      {
+        multiply: "5 x 10",
+        answer: "",
+      },
+    ],
+  },
+  {
+    container: [
+      {
+        multiply: "6 x 1",
+        answer: "",
+      },
+      {
+        multiply: "6 x 2",
+        answer: "",
+      },
+      {
+        multiply: "6 x 3",
+        answer: "",
+      },
+      {
+        multiply: "6 x 4",
+        answer: "",
+      },
+      {
+        multiply: "6 x 5",
+        answer: "",
+      },
+      {
+        multiply: "6 x 6",
+        answer: "",
+      },
+      {
+        multiply: "6 x 7",
+        answer: "",
+      },
+      {
+        multiply: "6 x 8",
+        answer: "",
+      },
+      {
+        multiply: "6 x 9",
+        answer: "",
+      },
+      {
+        multiply: "6 x 10",
+        answer: "",
+      },
+    ],
+  },
+  {
+    container: [
+      {
+        multiply: "7 x 1",
+        answer: "",
+      },
+      {
+        multiply: "7 x 2",
+        answer: "",
+      },
+      {
+        multiply: "7 x 3",
+        answer: "",
+      },
+      {
+        multiply: "7 x 4",
+        answer: "",
+      },
+      {
+        multiply: "7 x 5",
+        answer: "",
+      },
+      {
+        multiply: "7 x 6",
+        answer: "",
+      },
+      {
+        multiply: "7 x 7",
+        answer: "",
+      },
+      {
+        multiply: "7 x 8",
+        answer: "",
+      },
+      {
+        multiply: "7 x 9",
+        answer: "",
+      },
+      {
+        multiply: "7 x 10",
+        answer: "",
+      },
+    ],
+  },
+  {
+    container: [
+      {
+        multiply: "8 x 1",
+        answer: "",
+      },
+      {
+        multiply: "8 x 2",
+        answer: "",
+      },
+      {
+        multiply: "8 x 3",
+        answer: "",
+      },
+      {
+        multiply: "8 x 4",
+        answer: "",
+      },
+      {
+        multiply: "8 x 5",
+        answer: "",
+      },
+      {
+        multiply: "8 x 6",
+        answer: "",
+      },
+      {
+        multiply: "8 x 7",
+        answer: "",
+      },
+      {
+        multiply: "8 x 8",
+        answer: "",
+      },
+      {
+        multiply: "8 x 9",
+        answer: "",
+      },
+      {
+        multiply: "8 x 10",
+        answer: "",
+      },
+    ],
+  },
+  {
+    container: [
+      {
+        multiply: "9 x 1",
+        answer: "",
+      },
+      {
+        multiply: "9 x 2",
+        answer: "",
+      },
+      {
+        multiply: "9 x 3",
+        answer: "",
+      },
+      {
+        multiply: "9 x 4",
+        answer: "",
+      },
+      {
+        multiply: "9 x 5",
+        answer: "",
+      },
+      {
+        multiply: "9 x 6",
+        answer: "",
+      },
+      {
+        multiply: "9 x 7",
+        answer: "",
+      },
+      {
+        multiply: "9 x 8",
+        answer: "",
+      },
+      {
+        multiply: "9 x 9",
+        answer: "",
+      },
+      {
+        multiply: "9 x 10",
+        answer: "",
+      },
+    ],
+  },
+  {
+    container: [
+      {
+        multiply: "10 x 1",
+        answer: "",
+      },
+      {
+        multiply: "10 x 2",
+        answer: "",
+      },
+      {
+        multiply: "10 x 3",
+        answer: "",
+      },
+      {
+        multiply: "10 x 4",
+        answer: "",
+      },
+      {
+        multiply: "10 x 5",
+        answer: "",
+      },
+      {
+        multiply: "10 x 6",
+        answer: "",
+      },
+      {
+        multiply: "10 x 7",
+        answer: "",
+      },
+      {
+        multiply: "10 x 8",
+        answer: "",
+      },
+      {
+        multiply: "10 x 9",
+        answer: "",
+      },
+      {
+        multiply: "10 x 10",
+        answer: "",
+      },
+    ],
+  }
+];
+
+const gridContainer = document.querySelector("#grid-container");
+
+multiplication.forEach((containers) => {
+
+  const card = document.createElement("div");
+  card.classList.add("card")
+  gridContainer.appendChild(card);
+
+  containers.container.forEach((item) => {
+
+    const row = document.createElement("div");
+    row.classList.add("row-wrapper")
+    const number = document.createElement("span");
+    number.textContent = item.multiply;
+    const equal = document.createElement("span");
+    equal.textContent = "=";
+    const input = document.createElement("input");
+    input.type = "text";
+    row.append(number, equal, input);
+
+    card.appendChild(row);
+  });
+});
+
+const CheckButton = document.createElement("button")
+CheckButton.textContent = "Check answer"
+const resetButton = document.createElement("button")
+resetButton.textContent = "Reset"
+gridContainer.append(CheckButton, resetButton)
+
