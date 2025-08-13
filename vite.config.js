bc39429086439b8d2827d1288158e5f382c4ec47
Paulo Silva <paulofernando78@gmail.com>
2025-08-13 03:12:28 -0300

    wip

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        matematica: path.resolve(__dirname, 'src/materia/matematica.html'),
+        tabuada: path.resolve(__dirname, 'src/materia/matematica/tabuada.html'),
+      }
+    },
+    outDir: 'dist',
+  },
+})
